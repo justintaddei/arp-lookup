c2d5690fd3265641de04c02f18449f580dff4af2
Alessio Carrafa <a.carrafa@inglobetechnologies.com>
2019-10-13 15:01:54 +0200

    updated mock and test

--- __test__/arp-lookup.test.ts
+++ __test__/arp-lookup.test.ts
@@ -4,20 +4,20 @@ import arp from '../src/index'
 
 test('Arp table is parsed', async done => {
   await expect(arp.getTable()).resolves.toEqual([
-    { ip: '192.168.137.255', mac: 'ff:ff:ff:ff:ff:ff', type: 'static' },
-    { ip: '224.0.0.22', mac: '01:00:5e:00:00:16', type: 'static' },
-    { ip: '224.0.0.251', mac: '01:00:5e:00:00:fb', type: 'static' },
-    { ip: '224.0.0.252', mac: '01:00:5e:00:00:fc', type: 'static' },
-    { ip: '239.255.255.250', mac: '01:00:5e:7f:ff:fa', type: 'static' },
-    { ip: '255.255.255.255', mac: 'ff:ff:ff:ff:ff:ff', type: 'static' },
-    { ip: '192.168.2.1', mac: '04:a1:51:1b:12:92', type: 'dynamic' },
-    { ip: '192.168.2.255', mac: 'ff:ff:ff:ff:ff:ff', type: 'static' },
-    { ip: '224.0.0.2', mac: '01:00:5e:00:00:02', type: 'static' },
-    { ip: '224.0.0.22', mac: '01:00:5e:00:00:16', type: 'static' },
-    { ip: '224.0.0.251', mac: '01:00:5e:00:00:fb', type: 'static' },
-    { ip: '224.0.0.252', mac: '01:00:5e:00:00:fc', type: 'static' },
-    { ip: '239.255.255.250', mac: '01:00:5e:7f:ff:fa', type: 'static' },
-    { ip: '255.255.255.255', mac: 'ff:ff:ff:ff:ff:ff', type: 'static' }
+    { ip: '192.168.137.255', mac: 'ff:ff:ff:ff:ff:ff', type: 'static', vendor: '' },
+    { ip: '224.0.0.22', mac: '01:00:5e:00:00:16', type: 'static', vendor: '' },
+    { ip: '224.0.0.251', mac: '01:00:5e:00:00:fb', type: 'static', vendor: '' },
+    { ip: '224.0.0.252', mac: '01:00:5e:00:00:fc', type: 'static', vendor: '' },
+    { ip: '239.255.255.250', mac: '01:00:5e:7f:ff:fa', type: 'static', vendor: '' },
+    { ip: '255.255.255.255', mac: 'ff:ff:ff:ff:ff:ff', type: 'static', vendor: '' },
+    { ip: '192.168.2.1', mac: '04:a1:51:1b:12:92', type: 'dynamic', vendor: 'Netgear' },
+    { ip: '192.168.2.255', mac: 'ff:ff:ff:ff:ff:ff', type: 'static', vendor: '' },
+    { ip: '224.0.0.2', mac: '01:00:5e:00:00:02', type: 'static', vendor: '' },
+    { ip: '224.0.0.22', mac: '01:00:5e:00:00:16', type: 'static', vendor: '' },
+    { ip: '224.0.0.251', mac: '01:00:5e:00:00:fb', type: 'static', vendor: '' },
+    { ip: '224.0.0.252', mac: '01:00:5e:00:00:fc', type: 'static', vendor: '' },
+    { ip: '239.255.255.250', mac: '01:00:5e:7f:ff:fa', type: 'static', vendor: '' },
+    { ip: '255.255.255.255', mac: 'ff:ff:ff:ff:ff:ff', type: 'static', vendor: '' }
   ])
   done()
 })
